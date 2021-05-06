3589669b4c0d749b7c026f5b7f2f663d3032bbbb
Gillian <75751732+gillianzhaoxz@users.noreply.github.com>
2021-05-05 23:00:44 -0500

    Add files via upload

---
+++ assets/doc/SF-TOD-DEV/WebMap/js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#button1').click(function(e){
+        window.location.href="./main.html";
+    })
+})
+
+$(document).ready(function(){
+    $('.toast').toast('show');
+  });  
